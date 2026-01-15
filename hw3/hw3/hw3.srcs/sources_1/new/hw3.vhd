cb2db8a7d4102840b9c6ae5388e02ac1c38450b1
JaydenRandolph <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2026-01-15 10:56:53 -0700

    hw3 submission

---
+++ hw3/hw3/hw3.srcs/sources_1/new/hw3.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/15/2026 10:31:37 AM
+-- Design Name: 
+-- Module Name: hw3 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hw3 is
+    Port ( d : in STD_LOGIC_VECTOR (7 downto 0);
+           h : out STD_LOGIC);
+end hw3;
+
+architecture Behavioral of hw3 is
+
+begin
+
+    h <= '1' when d = x"00" or d = x"11" or d = x"22" or d = x"33" or d = x"44" or d = x"55" or d = x"66" or d = x"77" or d = x"88" or d = x"99" or d = x"AA" or d = x"BB" or d = x"CC" or d = x"DD" or d = x"EE" or d = x"FF" else
+         '0';
+
+end Behavioral;
---
+++ hw3/hw3/hw3.srcs/sources_1/new/hw3_tb.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/15/2026 10:31:37 AM
+-- Design Name: 
+-- Module Name: hw3_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hw3_tb is
+--  Port ( );
+end hw3_tb;
+
+architecture Behavioral of hw3_tb is
+
+    --the hw3 "box" I built and want to test
+    component hw3
+        Port ( d : in STD_LOGIC_VECTOR (7 downto 0);
+               h : out STD_LOGIC);
+    end component;
+    
+    --wires that will hook up to the hw3 "box" I want to test
+    signal d_tb : std_logic_vector(7 downto 0);
+    signal h_tb : std_logic;
+
+begin
+
+--wires up the "test switches" to the hw3 "box" I'm testing
+uut: hw3 port map(
+        d => d_tb,
+        h => h_tb
+    );
+    
+    
+-- "hands flipping the switches" part of my code
+    stim_proc: process
+    begin
+    --Used ChatGPT past here to make my test cases
+        -- multiple of 17 → LED should be on
+        d_tb <= x"00"; wait for 10 ns;
+        d_tb <= x"11"; wait for 10 ns;
+        d_tb <= x"22"; wait for 10 ns;
+        d_tb <= x"33"; wait for 10 ns;
+        d_tb <= x"44"; wait for 10 ns;
+        d_tb <= x"55"; wait for 10 ns;
+        d_tb <= x"66"; wait for 10 ns;
+        d_tb <= x"77"; wait for 10 ns;
+    
+        -- not multiples of 17 → LED should be off
+        d_tb <= x"01"; wait for 10 ns;
+        d_tb <= x"10"; wait for 10 ns;
+        d_tb <= x"20"; wait for 10 ns;
+        d_tb <= x"7F"; wait for 10 ns;
+
+    wait; -- stop simulation
+end process;
+        
+
+
+end Behavioral;
