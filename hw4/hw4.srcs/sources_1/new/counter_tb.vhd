fc8914f4830abb0204d815471ec1e4298756606a
JaydenRandolph <177895387+JaydenRandolph@users.noreply.github.com>
2026-01-22 08:03:21 -0700

    Updated counter_tb.vhd to match with my file

--- hw4/hw4.srcs/sources_1/new/counter_tb.vhd
+++ hw4/hw4.srcs/sources_1/new/counter_tb.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 01/20/2026 08:57:06 AM
 -- Design Name: 
--- Module Name: counter_tb - Behavioral
+-- Module Name: counter - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -24,20 +24,47 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity counter_tb is
---  Port ( );
-end counter_tb;
+entity counter is
+    generic (
+           num_bits : integer := 4;
+           max_value : integer := 9
+    );
+    port ( clk : in STD_LOGIC;
+           reset_n : in STD_LOGIC;
+           ctrl : in STD_LOGIC;
+           roll : out STD_LOGIC;
+           Q : out unsigned (num_bits-1 downto 0));
+end counter;
 
-architecture Behavioral of counter_tb is
+architecture Behavioral of counter is
+
+    --the wires inside the entity; what will wire into the outputs of the box
+    signal processQ: unsigned(num_bits-1 downto 0); --cannot do math with a port - must be a wire
+begin
+
+process(clk)
 
 begin
+    if(rising_edge(clk)) then
+        roll <= '0'; --defaults roll to 0
+        if(reset_n = '0') then
+            processQ <= (others => '0'); --resets Q
+        elsif((processQ < max_value)and (reset_n = '1') and (ctrl = '1')) then
+            processQ <= processQ + 1;
+        elsif((processQ = max_value)and (reset_n = '1') and (ctrl = '1')) then
+            processQ <= (others => '0');
+            roll <= '1'; 
+        end if;
+    end if;
+end process;
+Q <= processQ;
 
 
 end Behavioral;
